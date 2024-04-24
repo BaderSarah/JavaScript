ec07502d34ba9d243a83b22a43bb2defaf5291e5
BaderSarah <bader.sarah@icloud.com>
2024-04-24 17:18:06 +0200

    working on js code

--- src/CoursesComponent.js
+++ src/CoursesComponent.js
@@ -1,26 +1,58 @@
-import CoursesRepository from "./CoursesRepository.js";
+import CoursesRepository from "../CoursesRepository.js";
 
 export default class CoursesComponent {
+  #names = [];
   #storage;
   #coursesRepository;
 
   constructor() {
     this.#coursesRepository = new CoursesRepository();
+    this.#storage = window.localStorage;
+
+    document.getElementById("add").onclick = () => {
+      document.getElementById("error").innerText = "";
+      const name = document.getElementById("inputName").value;
+      if (
+        name == "" ||
+        this.#names.find((n) => n.toUpperCase() === name.toUpperCase())
+      ) {
+        document.getElementById("error").innerText =
+          "Course name is already in use or empty";
+      } else {
+        this.#addNameCourse(name);
+      }
+      document.getElementById("inputName").value = ""; //tektsfiel import dan moet je de value property gebruiken
+      console.log(this.#names);
+    };
+
+    this.#toHtml();
   }
 
-  #initializeHTML() {}
+  #addNameCourse(name) {
+    this.#names.push(name);
+    this.#setNamesInStorage();
+    //this.#toHtml();
+  }
+
+  #toHtml() {
+    this.#courseNamesToHtml();
+    this.#coursesToHtml();
+  }
+
+  #setNamesInStorage() {
+    this.#storage.setItem("namesCourses", JSON.stringify(this.#names));
+  }
+
+  #getNamesInStorage() {
+    this.#names = [];
+    if (this.#storage.getItem("namesCourses")) {
+      this.#names = JSON.parse(this.#storage.getItem("namesCourses"));
+    }
+  }
 
   #courseNamesToHtml() {}
 
-  #courseDetailsToHtml() {}
-
-  #addCourseToHtml() {
-    const addBtn = document.getElementById("add");
-    addBtn.onclick = () => {
-      const name = document.getElementById("name").value;
-      this.#coursesRepository.addCourse(name);
-    };
-  }
+  #coursesToHtml() {}
 
   #addToDosToHtml() {}
 }
